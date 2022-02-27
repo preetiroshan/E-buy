47108884b8bc3aa67cc8210de2d24c93ccf396b3
preetiroshan <preetiroshan14@gmail.com>
2022-02-27 19:44:20 +0530

    Update server.js

--- Server/src/server.js
+++ Server/src/server.js
@@ -35,4 +35,4 @@ app.use((err, req, res, next) => {
     res.status(500).send({ message: err.message })
 })
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}. ${process.env.MONGODB_URL}, ${connectionString}`));
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
